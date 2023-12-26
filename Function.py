8e5be0e06b16bece3052e93ad7fa62ab7c3cea22
GuilleDiazFentanes <152492889+GuilleDiazFentanes@users.noreply.github.com>
2023-12-26 04:53:37 +0100

    Update Function.py

--- Function.py
+++ Function.py
@@ -12,16 +12,19 @@ url = (f'https://discord.com/api/v8/channels/{Vars.channelId}/messages')
 
 def simpleRoll():
 
-    print('Starting to Roll')
+    print (time.strftime("Rolling at %H:%M - %d/%m/%y",time.localtime()))
     i = 1
     x = 0
+    claimed = '❤️ '
+    unclaimed = '🤍'
+    kakera = '💎'
     rollCommand = SlashCommander(bot.getSlashCommands(botID).json()).get([Vars.rollCommand])
     continueRolling = True
 
-    while continueRolling == True or x < 4:
+    while continueRolling == True or x < 3:
 
         bot.triggerSlashCommand(botID, Vars.channelId, Vars.serverId, data = rollCommand)
-        time.sleep(1.2)
+        time.sleep(1.8)
         r = requests.get(url , headers = auth )
         jsonCard = json.loads(r.text)
 
@@ -33,27 +36,28 @@ def simpleRoll():
         try:
             cardName = (jsonCard[0]['embeds'][0]['author']['name'])
             cardSeries = (jsonCard[0]['embeds'][0]['description']).replace('\n', '**').split('**')[0]
-            print(i,' ---- '+cardName+' - '+ cardSeries) 
         except IndexError:
             cardName = 'null'
             cardSeries = 'null'
 
+        if not 'footer' in jsonCard[0]['embeds'][0] or not 'icon_url' in jsonCard[0]['embeds'][0]['footer']:
+            print(i,' - '+unclaimed+' ---- '+cardName+' - '+cardSeries)
+            if cardSeries in Vars.desiredSeries:
+                print('Trying to Claim '+ cardName)
+                r= requests.put(f'https://discord.com/api/v8/channels/{Vars.channelId}/messages/{idMessage}/reactions/🐿️/%40me',headers=auth)
+        else: 
+            print(i,' - '+claimed+' ---- '+cardName+' - '+cardSeries)
+
         try:
             cardsKakera = (jsonCard[0]['components'][0]['components'][0]['emoji']['name'])
-            print(cardsKakera)
-            print(Vars.desiredKakeras)
             if cardsKakera in Vars.desiredKakeras:
-                print('Trying to react to '+ cardsKakera)
+                x -= 1 
+                print(kakera+' - '+kakera+' - Trying to react to '+ cardsKakera+ ' of '+ cardName)
                 bot.click( jsonCard[0]['author']['id'], channelID =jsonCard[0]['channel_id'], guildID = Vars.serverId, messageID=jsonCard[0]['id'], messageFlags=jsonCard[0]['flags'], data={'component_type': 2, 'custom_id': jsonCard[0]['components'][0]['components'][0]['custom_id']},)
 
         except IndexError:
             cardsKakera = 'null'
 
-        if cardSeries in Vars.desiredSeries:
-            if not 'footer' in jsonCard[0]['embeds'][0] or not 'icon_url' in jsonCard[0]['embeds'][0]['footer']:
-                print('Trying to Claim '+ cardName)
-                r= requests.put(f'https://discord.com/api/v8/channels/{Vars.channelId}/messages/{idMessage}/reactions/🐿️/%40me',headers=auth)
-                time.sleep(2.6)
         i += 1
     print('Rolling ended')
 
